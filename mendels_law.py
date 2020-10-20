6c0381531c92dd33e28952a3e6c97424ae089128
bjralexandru <bjalexandru@gmail.com>
2020-10-20 20:05:27 +0300

    Expected Offspring Simple Problem

--- mendels_law.py
+++ mendels_law.py
@@ -11,19 +11,19 @@ a dominant allele (and thus displaying the dominant phenotype). Assume that any 
 
 
 def dominant_probability(num_homozygous_dominant, num_heterozygous, num_homozygous_recessive): 
-	total = num_homozygous_dominant + num_heterozygous + num_homozygous_recessive # Get the total population
+    total = num_homozygous_dominant + num_heterozygous + num_homozygous_recessive # Get the total population
 
 # 	Every posssible combination from mating 2 'n' organisms will be recessive
 
     recessive_probability = (num_homozygous_recessive / total) * ((num_homozygous_recessive - 1) / (total - 1))
     
 # By mating 'm' organisms, recessive outsprings will account for 25% of the total cases.    
-	heterozygous_probability = (num_heterozygous / total) * ((num_heterozygous - 1) / (total - 1))
-	hetero_recessive_probability = (num_heterozygous / total) * (num_homozygous_recessive / (total - 1)) + (num_homozygous_recessive / total) * (num_heterozygous / (total - 1))
+    heterozygous_probability = (num_heterozygous / total) * ((num_heterozygous - 1) / (total - 1))
+    hetero_recessive_probability = (num_heterozygous / total) * (num_homozygous_recessive / (total - 1)) + (num_homozygous_recessive / total) * (num_heterozygous / (total - 1))
 
 # When mating 'm' and 'n' organisms 50% of the results will be recessive
-	recessive_total = recessive_probability + heterozygous_probability * (.25) + hetero_recessive_probability * (.5)
-	return (1 - recessive_total) # To get the answer we will subtract all the recessives from 100%
+    recessive_total = recessive_probability + heterozygous_probability * (.25) + hetero_recessive_probability * (.5)
+    return (1 - recessive_total) # To get the answer we will subtract all the recessives from 100%
 
 
 
